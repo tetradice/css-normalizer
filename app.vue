1a856d7802d95ea22452143f3dcb9185a271244f
Dice <tetradice@gmail.com>
2025-02-08 09:07:26 +0900

    テキストエリア配置、tailwind css有効化

--- app.vue
+++ app.vue
@@ -1,7 +1,28 @@
+<script lang="ts" setup>
+const source = ref("");
+const output = ref("");
+</script>
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-    <Button label="Verify"></Button>
+    <div class="grid grid-cols-2 gap-4">
+      <FloatLabel variant="on" class="col">
+        <Textarea id="over_label" v-model="source" rows="5" cols="30" />
+        <label for="on_label">正規化したいCSS/SCSSを入力してください</label>
+      </FloatLabel>
+
+      <FloatLabel variant="on" class="col">
+        <Textarea
+          id="over_label"
+          v-model="source"
+          rows="5"
+          cols="30"
+          class="!col"
+        />
+        <label for="on_label">正規化結果</label>
+      </FloatLabel>
+    </div>
   </div>
 </template>
+
+<style lang="css"></style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,7 +4,8 @@ import Aura from "@primevue/themes/aura";
 export default defineNuxtConfig({
   compatibilityDate: "2024-11-01",
   devtools: { enabled: true },
-  modules: ["@primevue/nuxt-module"],
+  modules: ["@primevue/nuxt-module", "@nuxtjs/tailwindcss"],
+  tailwindcss: {},
   primevue: {
     options: {
       theme: {
